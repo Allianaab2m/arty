68762f2e4ffdc403e26cecbde11ea6566c7d136d
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-05-25 10:43:04 +0900

    feat: dialogue

---
+++ src/dialogue.ts
@@ -0,0 +1,19 @@
+const dialogue = {
+  error: {
+    argsError: "引数が足りないみたい。もう一回確かめてほしい。",
+    commandNotFound: "コマンドが存在しません。",
+    unexpected: "予期しないエラーが起こりました。"
+  },
+  contact: {
+    notDirect: "その話はDMで話したほうが良さそう。ちょっと待ってね。",
+    directCallback: "お待たせしました。このノートにリプライで内容を送ってね。",
+    accept: "問い合わせを受け付けたよ。ありがとうございました！"
+  },
+  greeting: {
+    morning: "おはようございます、{{name}}さん！",
+    hello: "こんにちは、{{name}}さん！",
+    evening: "こんばんは、{{name}}さん！"
+  }
+}
+
+export default dialogue
