9b1db1b67bc3db94744171d580221952734d7a50
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-06-26 20:59:15 +0900

    chore: ci testing

--- src/libs/misskey_test.ts
+++ src/libs/misskey_test.ts
@@ -3,15 +3,5 @@ import * as mf from 'mf'
 
 Deno.test('test #1', async () => {
   mf.install()
-  mf.mock('GET@/blog', (_req, params) => {
-    return new Response('externalCallA', {
-      status: 200,
-    })
-  })
-
-  const res = await fetch('https://example.com/blog')
-  const text = await res.text()
-
-  assertEquals(text, 'externalCallA')
   mf.uninstall()
 })
